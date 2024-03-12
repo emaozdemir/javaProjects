18e16ca46815173cb6b5c03d66c16362d3648afe
emaozdemir <semaozdemiraslan@gmail.com>
2024-03-12 02:53:50 +0100

    12.03

--- src/P03/kahve/Kahve.java
+++ src/P03/kahve/Kahve.java
@@ -1,5 +1,7 @@
 package P03.kahve;
 
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -61,12 +63,106 @@ public class Kahve {
                 c_GREEN + " 3.Espresso");
 
         int tercih = input.nextInt();
-        if (tercih ==1) {//Turk kahvesitercih action kontrol
-            System.out.println(kahveListesi.get(tercih-1)+" hazırlanıyor");
-            coffeeName=kahveListesi.get(tercih-1);
-            price=30;
-
+        if (tercih == 1) {//Turk kahvesitercih action kontrol
+            System.out.println(kahveListesi.get(tercih - 1) + " hazırlanıyor");
+            coffeeName = kahveListesi.get(tercih - 1);
+            price = 30;
+        } else if (tercih == 2) {//
+            System.out.println(kahveListesi.get(tercih - 1) + " hazırlanıyor");
+            coffeeName = kahveListesi.get(tercih - 1);
+            price = 35;
+        } else if (tercih == 3) {//
+            System.out.println(kahveListesi.get(tercih - 1) + " hazırlanıyor");
+            coffeeName = kahveListesi.get(tercih - 1);
+            price = 40;
+        } else {
+            System.out.println("Hatalı secim yaptınız . tekrar deneyiniz.");
+            kahveSec();
         }
+        sutSec();
+
+
     }
 
+    // todo....... SUT METHOD.....
+    public void sutSec() {
+        System.out.println(BOLD + "Kahvenize SÜT eklemek ister misiniz? \n"
+                + c_GREEN + "sut tercihiniz EVET ise -> E\n"
+                + c_RED + "sut tercihiniz Hayır  ise -> H\ngiriniz : ");
+        String sutTercih = input.next().substring(0, 1);
+        if (sutTercih.equalsIgnoreCase("E")) {
+            System.out.println(BOLD + "Kahvenize SÜT ekleniyor");
+            milk = "SÜTLÜ GAAVEEE :) ";
+            price += 10;
+        } else if (sutTercih.equalsIgnoreCase("H")) {
+            System.out.println(BOLD + "Kahveniz SÜT süz hazırlanıyor");
+            milk = "SADEli GAAVEE  :( ";
+        } else {
+            System.out.println("Hatalı secim yaptınız . tekrar deneyiniz.");
+            sutSec();
+        }
+        sekerSec();
+    }
+
+    // todo....... SEKER METHOD.....
+    public void sekerSec() {
+        System.out.println(BOLD + "Kahvenize SEKER eklemek ister misiniz? \n"
+                + c_GREEN + "seker tercihiniz EVET ise -> E\n"
+                + c_RED + "seker tercihiniz Hayır  ise -> H\ngiriniz : ");
+        String sekerTercih = input.next().substring(0, 1);
+        if (sekerTercih.equalsIgnoreCase("E")) {
+            System.out.println("Kac seker alırsınız : ");
+            int sekerSayisi = input.nextInt();
+            System.out.println(BOLD + "Kahvenize " + sekerSayisi + " tane seker  ekleniyor");
+            sugar = "Sekerli GAAVEEE :( ";
+
+        } else if (sekerTercih.equalsIgnoreCase("H")) {
+            System.out.println(BOLD + "Kahveniz SEKERSİZ hazırlanıyor");
+            sugar = "Sekersiz GAAVEEE :) ";
+        } else {
+            System.out.println("Hatalı secim yaptınız . tekrar deneyiniz.");
+            sekerSec();
+        }
+
+        boyutSec();
+    }
+    // todo....... KAHVE SIZE METHOD.....
+
+    public void boyutSec() {
+        System.out.println(BOLD + "Kahvenize hangi boyda almak istersiniz? \n"
+                + c_GREEN + "1.BUYUK ise -> B\n"
+                + c_RED + "2.KÜÇÜK ise -> S\n"
+                + c_PURPLE + "3.ORTA ise -> M\n" + BOLD + "giriniz : ");
+        String boyTercih = input.next().substring(0, 1);
+        if (boyTercih.equalsIgnoreCase("B")) {
+            System.out.println(BOLD + "Kahveniz " + boyTercih + " Boy hazırlanıyor ");
+            coffeeSize = "BUYUK BOY GAAVEEE :) ";
+            price += 20;
+        } else if (boyTercih.equalsIgnoreCase("M")) {
+            System.out.println(BOLD + "Kahveniz " + boyTercih + " Boy hazırlanıyor ");
+            coffeeSize = "ORTA BOY GAAVEEE :) ";
+            price += 10;
+        } else if (boyTercih.equalsIgnoreCase("S")) {
+            System.out.println(BOLD + "Kahveniz " + boyTercih + " Boy hazırlanıyor ");
+            coffeeSize = "KUCUK BOY GAAVEEE :) ";
+
+        } else {
+            System.out.println("Hatalı secim yaptınız . tekrar deneyiniz.");
+            boyutSec();
+        }
+        ucretHesapla();
+    }
+
+    // todo....... KAHVE UCRET ODEME METHOD.....
+    public void ucretHesapla() {
+        System.out.println("Kahvenizin toplam ücreti : " + price + " TL hesaplanmıstır");
+        int odeme = input.nextInt();
+        System.out.println("Odeme için girilen tutar : " + odeme + " TL ");
+        if (price > odeme) {
+            System.out.println((price - odeme) + " TL kadar eksik odeme yaptınız");
+            ucretHesapla();
+        } else {
+            System.out.println("odeme için teşekkürler :) \npara üstü : " + (odeme - price) + " TL");
+        }
+    }
 }
--- src/P03/kahve/KahveRunner.java
+++ src/P03/kahve/KahveRunner.java
@@ -3,16 +3,44 @@ package P03.kahve;
 
 public class KahveRunner {
 
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      Kahve musteri1 = new Kahve();
-      System.out.println(musteri1);
-      musteri1.kahveSec();//objName ile meth. call
-      //  kahveSec(); -> static meth import veya calss name ile call edldi
-      System.out.println(musteri1);//
+        Kahve musteri1 = new Kahve();
 
 
-   }
+        //  Kahve musteri2 = new Kahve();
+        //  Kahve musteri3 = new Kahve();
+        System.out.println(musteri1);
+        musteri1.kahveSec();//objName ile meth. call
+        //  musteri2.kahveSec();//objName ile meth. call
+        //  musteri3.kahveSec();//objName ile meth. call
+        //  kahveSec(); -> static meth import veya calss name ile call edldi
+        System.out.println(musteri1);//
+        //  System.out.println(musteri2);//
+        //  System.out.println(musteri3);//
+        //  musteri1.sutSec();//objName ile meth. call
+        //  musteri2.sutSec();//objName ile meth. call
+        //  musteri3.sutSec();//objName ile meth. call
+        // System.out.println(musteri1);//
+        //  System.out.println(musteri2);//
+        //  System.out.println(musteri3);//
+        // musteri1.sekerSec();//objName ile meth. call
+        // musteri2.sekerSec();//objName ile meth. call
+        // musteri3.sekerSec();//objName ile meth. call
+        // System.out.println(musteri1);
+        // System.out.println(musteri2);
+        // System.out.println(musteri3);
+        // musteri1.boyutSec();
+        // musteri2.boyutSec();
+        // musteri3.boyutSec();
+        //System.out.println(musteri1);
+        // System.out.println(musteri2);
+        // System.out.println(musteri3);
+        //  musteri1.ucretHesapla();
+        //  musteri2.ucretHesapla();
+        //  musteri3.ucretHesapla();
+
+    }
 
 
 }
